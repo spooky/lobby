ed4a251c55dc54f9ee18b3fc112f3dd427351200
Michael Sondergaard <msond@cs.au.dk>
2014-10-23 15:37:39 +0200

    Use a triple for describing specific fmod versions

--- src/fa/featured.py
+++ src/fa/featured.py
@@ -4,8 +4,12 @@ import os
 import git
 import util
 import logging
+from collections import namedtuple
 
 logger = logging.getLogger(__name__)
+
+
+FeaturedModVersion = namedtuple('FeaturedModVersion', ['repo', 'tag', 'hash'])
 
 def checkout_featured_mod(featured_mod, featured_repo, featured_version="faf/master", repo_dir=util.REPO_DIR):
     mod_repo = git.Repository(os.path.join(repo_dir, featured_mod), featured_repo)
@@ -14,3 +18,8 @@ def checkout_featured_mod(featured_mod, featured_repo, featured_version="faf/mas
 
 def featured_versions_to_repo_tag(featured_versions_hash):
     return str(reduce(max, featured_versions_hash.itervalues(), 0))
+
+def replay_info_to_featured_mod_version(replay_info):
+    return FeaturedModVersion("FAForever/fa",
+                              featured_versions_to_repo_tag(replay_info["featured_mod_versions"]),
+                              None)
