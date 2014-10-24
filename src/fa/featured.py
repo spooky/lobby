e0c504fa2058f4e432044d6d7a4ebbb51015bd0f
Michael Sondergaard <msond@cs.au.dk>
2014-10-24 23:03:43 +0200

    Make featured.py use git/version

--- src/fa/featured.py
+++ src/fa/featured.py
@@ -5,7 +5,8 @@ import git
 import util
 import logging
 import urllib2
-from collections import namedtuple
+
+from git import Version
 
 logger = logging.getLogger(__name__)
 
@@ -17,8 +18,6 @@ FEATURED_MOD_TO_REPO_NAME = {
 
 DEFAULT_REPO_BASE = "FAForever"
 
-FeaturedModVersion = namedtuple('FeaturedModVersion', ['repo', 'tag', 'hash'])
-
 def checkout_featured_mod(featured_mod, featured_repo, featured_version="faf/master", repo_dir=util.REPO_DIR):
     mod_repo = git.Repository(os.path.join(repo_dir, featured_mod), featured_repo)
     mod_repo.fetch()
@@ -28,6 +27,6 @@ def featured_versions_to_repo_tag(featured_versions_hash):
     return str(reduce(max, featured_versions_hash.itervalues(), 0))
 
 def replay_info_to_featured_mod_version(replay_info):
-    return FeaturedModVersion("/".join([DEFAULT_REPO_BASE, FEATURED_MOD_TO_REPO_NAME[replay_info["featured_mod"]]]),
+    return Version("/".join([DEFAULT_REPO_BASE, FEATURED_MOD_TO_REPO_NAME[replay_info["featured_mod"]]]),
                               featured_versions_to_repo_tag(replay_info["featured_mod_versions"]),
                               None)
