54b7af4d62a3eff6c4222a18c13f032428f44980
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2015-02-20 23:59:32 +0100

    signal makers

--- src/view_models/adapters.py
+++ src/view_models/adapters.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QAbstractItemModel, QModelIndex, pyqtSlot
+from PyQt5.QtCore import QAbstractItemModel, QModelIndex, QObject, pyqtSlot, pyqtProperty, pyqtSignal, pyqtWrapperType
 
 
 def ListModelFor(klass):
@@ -55,3 +55,72 @@ def ListModelFor(klass):
             item.setProperty(prop, value)
 
     return ListModel
+
+
+class notifyableProperty:
+
+    """
+        Just a placeholder, it gets replaced with pyqtProperty
+        when creating the object. Used to customise signal name.
+    """
+
+    def __init__(self, property_type, signal_name=None):
+        self.property_type = property_type
+        self.signal_name = signal_name
+
+
+class notifyablePropertyWrapperType(pyqtWrapperType):
+
+    ''' Generates a *_changed signal for each pyqtProperty found for a class '''
+
+    def __new__(meta, name, bases, dct):
+
+        def notifyable_property(prop_name, prop_type, notify, notify_name):
+            """
+                Create pyqtProperty object with value captured in the closure
+                and a 'notify' signal attached.
+
+                You need to pass both:
+                    - unbound signal, because pyqtProperty needs it
+                    - signal name to allow setter to find the *bound* signal
+                      and emit it.
+            """
+
+            def getter(self):
+                return getattr(self, '__' + prop_name)
+
+            def setter(self, value):
+                setattr(self, '__' + prop_name, value)
+                getattr(self, notify_name).emit(value)
+
+            return pyqtProperty(type=prop_type, fget=getter, fset=setter, notify=notify)
+
+        # don't touch attributes other than notifyableProperties
+        properties = list(
+            filter(
+                lambda i: isinstance(i[1], notifyableProperty),
+                dct.items()
+            )
+        )
+
+        for property_name, p in properties:
+            signal_name = p.signal_name or property_name + '_changed'
+            signal = pyqtSignal(p.property_type, name=signal_name)
+
+            # create dedicated signal for each property
+            dct[signal_name] = signal
+
+            # create property backing field
+            dct['__' + property_name] = None
+
+            # substitute notifyableProperty placeholder with real pyqtProperty
+            dct[property_name] = notifyable_property(property_name, p.property_type, signal, signal_name)
+
+        return super().__new__(meta, name, bases, dct)
+
+
+class NotifyablePropertyObject(QObject, metaclass=notifyablePropertyWrapperType):
+
+    ''' Base class for model objects. This class uses PropertyChangeSignalMeta to generate signals for each pyqtProperty found in a class '''
+
+    pass
---
+++ tests/src/test_adapters.py
@@ -0,0 +1,34 @@
+def test_notifyableProperty():
+    from view_models.adapters import NotifyablePropertyObject, notifyableProperty
+
+    class TestSubject(NotifyablePropertyObject):
+        name = notifyableProperty(str)
+        code = notifyableProperty(str, signal_name='kung_foo')
+
+        def __init__(self, name=None, code=None, parent=None):
+            super().__init__(parent)
+            self.name = name
+            self.code = code
+
+            self.name_changed.connect(self.on_name_changed)
+
+        def on_name_changed(self):
+            nonlocal was_on_name_changed_called
+            was_on_name_changed_called = True
+
+    was_on_name_changed_called = False  # poor mans method call check
+
+    t = TestSubject('first', 'second')
+    q = TestSubject('not first', 'not second')
+
+    assert t.name == 'first'
+    assert t.code == 'second'
+    assert hasattr(t, 'name_changed')   
+    assert hasattr(t, 'kung_foo')
+
+    t.name = 'changed'
+    assert t.name == 'changed'
+    assert was_on_name_changed_called
+
+    t.code = 'code_changed'
+    assert t.code == 'code_changed'
